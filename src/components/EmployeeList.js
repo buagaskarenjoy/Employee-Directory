a73dfff4d213f6ae6f81e965bbd44acb12332eaa
Karen Joy <karenjoybuagas@gmail.com>
2025-04-06 19:37:34 +0800

    search-branch

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./EmployeeList.css";
 
 const EmployeeList = ({ employees }) => {
     const navigate = useNavigate();
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const filteredEmployees = employees.filter((emp) =>
+        Object.values(emp).some((value) =>
+            value.toString().toLowerCase().includes(searchTerm.toLowerCase())
+        )
+    );
   
     return (
         <div className="employee-container">
             <button className="back-button" onClick={() => navigate("/home")}>⬅ Back to Home</button>
             <h3 className="page-title">📋 Employee List</h3>
+
+            <input
+                type="text"
+                className="search-bar"
+                placeholder="Search Employees..."
+                value={searchTerm}
+                onChange={(e)=> setSearchTerm(e.target.value)}   
+            />
+
+
             <div className="table-container">
                 <table className="employee-table">
                     <thead>
@@ -22,7 +39,7 @@ const EmployeeList = ({ employees }) => {
                         </tr>
                     </thead>
                     <tbody>
-                        {employees.map((emp) => (
+                        {filteredEmployees.map((emp) => (
                             <tr key={emp.id}>
                                 <td>{emp.id}</td>
                                 <td>{emp.name}</td>
